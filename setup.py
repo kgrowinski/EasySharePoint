f0faf4ec3a4eb743fe162f49f83be1730c6b304a
LZR3NN <k.growinski@outlook.com>
2016-12-22 13:21:54 +0100

    adding pip installation capabilities

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easy_sharepoint',
+      version='0.1',
+      description='Python Libriary to connect and perform simple Sharepoint Lists Operation',
+      url='https://github.com/Landarell/EasySharePoint',
+      author='Flying Circus',
+      author_email='Krzysztof Growiński',
+      license='MIT',
+      packages=['easy_sharepoint'],
+      zip_safe=False)
