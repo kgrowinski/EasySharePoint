7b88a45844563180b0935ea0fd9e79c3dec1e518
LZR3NN <k.growinski@outlook.com>
2016-12-28 09:36:03 +0100

    Add custom query

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='easy_sharepoint',
+    version='0.1',
+    packages=['easy_sharepoint'],
+    url='',
+    license='MIT',
+    author='Krzysztof Growinski',
+    author_email='k.growisnski@outlook.com',
+    description='Sharepoint List Operations Python Libriary',
+    requires=[
+        "requests",
+        "requests_ntlm"
+    ]
+)
